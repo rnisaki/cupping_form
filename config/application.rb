c026a9bff08048a715606874bf24e0297c89f6c7
加成実咲 <kanari.misaki@kmMacBook-Pro.local>
2018-12-25 13:26:08 +0900

    time-zone追加

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module CuppingForm
 	
 	config.assets.initialize_on_precompile = false
 
+	config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
